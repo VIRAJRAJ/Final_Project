c6ecf444bc844dee27a7ed093677d27bd0da3d7c
vikas <vikasrajuinversity@gmail.com>
2017-07-13 17:19:18 -0700

    djangomodel Demoapp..

--- Django_Project/Demoapp/models.py
+++ Django_Project/Demoapp/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    Mobile = models.CharField(max_length=255) #2update Mobile = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
