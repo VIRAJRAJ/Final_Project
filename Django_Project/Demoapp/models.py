42a3b0db1d5d5a445d12503f5866beaccb33407f
vikas <vikasrajuinversity@gmail.com>
2017-07-21 14:01:50 -0700

    add some file contents

--- Django_Project/Demoapp/models.py
+++ Django_Project/Demoapp/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-
+import uuid
 # Create your models here.
 class UserModel(models.Model):
     email = models.EmailField()
@@ -19,3 +19,41 @@ class SessionToken(models.Model):
 
     def create_token(self):
         self.session_token = uuid.uuid4()
+
+
+class PostModel(models.Model):
+    user = models.ForeignKey(UserModel)
+    image = models.FileField(upload_to='user_images')
+    image_url = models.CharField(max_length=255)
+    caption = models.CharField(max_length=240)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    has_liked = False
+
+
+
+	def like_count(self):
+		return len(LikeModel.objects.filter(post=self))
+
+
+
+	def comments(self):
+		return CommentModel.objects.filter(post=self).order_by('-created_on')
+
+
+class LikeModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+
+
+
+class CommentModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	comment_text = models.CharField(max_length=555)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
